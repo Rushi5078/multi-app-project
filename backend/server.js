58dc95a86fd1efa18e0626201f7860b7670febc8
Rushi5078 <88040056+Rushi5078@users.noreply.github.com>
2025-11-18 12:39:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ const { Client } = require('pg');
 
 const app = express();
 
+// Default route (fixes "Cannot GET /")
+app.get('/', (req, res) => {
+    res.send("Backend is running successfully!");
+});
+
+// API route
 app.get('/api', async (req, res) => {
     res.json({ message: "Hello from Node.js Backend!" });
 });
